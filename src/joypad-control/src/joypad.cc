454047395cf280be7a011d77717bff2421b0b125
Francesco Argentieri <francesco.argentieri89@gmail.com>
2021-03-20 17:34:26 +0100

    Search for button

--- src/joypad-control/src/joypad.cc
+++ src/joypad-control/src/joypad.cc
@@ -1,13 +1,11 @@
 #include "joypad.h"
 
+#include <ros/console.h>
+#include <sensor_msgs/JointState.h>
+#include <sensor_msgs/JoyFeedbackArray.h>
+
 #include <iostream>
 #include <string>
-
-
-
-#include <ros/console.h>
-#include <sensor_msgs/JoyFeedbackArray.h>
-#include <sensor_msgs/JointState.h>
 
 #include "buttonsmap_ps3joy.h"
 #include "buttonsname.h"
@@ -15,8 +13,8 @@
 #include "joypad_thumbstick.h"
 #include "joypad_trigger.h"
 
-std::unordered_map<uint8_t, std::string> Joypad::triggers_{
-    {6, trigger::KL2}, {7, trigger::KR2}};
+std::unordered_map<uint8_t, std::string> Joypad::triggers_{{6, trigger::KL2},
+                                                           {7, trigger::KR2}};
 
 std::unordered_map<uint8_t, std::string> Joypad::thumbsticks_{
     {0, thumbstick::kL3},
@@ -39,15 +37,23 @@ std::unordered_map<uint8_t, std::string> Joypad::special_{
 
 Joypad::Joypad() {
   // Subscribe to the /joy topic for input from joystick
-  joy_subriber_ = node_handler_.subscribe<sensor_msgs::Joy>("joy", 10, &Joypad::controllerCallback);
+  joy_subriber_ = node_handler_.subscribe<sensor_msgs::Joy>(
+      "joy", 10, &Joypad::controllerCallback);
 }
 
 void Joypad::controllerCallback(const sensor_msgs::Joy::ConstPtr& msg) {
-  ROS_INFO_STREAM( "Joypad::controllerCallback" );
-  if(msg->buttons[PS3_BUTTON_SELECT]){
+  ROS_INFO_STREAM("Joypad::controllerCallback");
+
+  ROS_INFO_STREAM(msg->axes[PS3_AXIS_BUTTON_REAR_LEFT_2])
+
+
+  if(msg->axes[PS3_AXIS_STICK_LEFT_UPWARDS]){
+    ROS_DEBUG("Press %s", Joypad::thumbstick_[0].c_str());
+  }
+  if (msg->buttons[PS3_BUTTON_SELECT]) {
     ROS_DEBUG("Press %s", Joypad::special_[8].c_str());
   }
-  if(msg->buttons[PS3_BUTTON_START]){
+  if (msg->buttons[PS3_BUTTON_START]) {
     ROS_DEBUG("Press %s", Joypad::special_[10].c_str());
   }
 }
